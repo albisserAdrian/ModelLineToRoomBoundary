78a5c5f241770d12a790375e31dbcef207065806
AdrianAlbisser <adrian.albisser@gmail.com>
2018-03-20 23:25:30 +1100

    First commit

---
+++ ModelLineToRoomBoundary.cs
@@ -0,0 +1,41 @@
+public void ModelLineToRoomBoundary()
+{
+
+	UIDocument uidoc = this.ActiveUIDocument;
+	Document doc = uidoc.Document;
+			
+			
+	View activeView = doc.ActiveView;
+
+	Selection sel = uidoc.Selection;
+	PickFilter selFilter = new PickFilter();
+	IList<Element> pickedRef = sel.PickElementsByRectangle(selFilter,"Select lines");
+						
+    foreach (Element e in pickedRef)
+	{
+				
+		ModelCurve m = e as ModelCurve;
+		Curve c = m.GeometryCurve;
+		curves.Append(c);
+	}
+
+	using (Transaction transaction = new Transaction(doc))
+	{
+		transaction.Start("Create Rooms");
+		doc.Create.NewRoomBoundaryLines(activeView.SketchPlane, curves, activeView);
+		transaction.Commit();
+	}	
+}
+		
+public class PickFilter : ISelectionFilter
+{
+    public bool AllowElement(Element e)
+    {
+        return (e.Category.Id.IntegerValue.Equals(
+        (int)BuiltInCategory.OST_Lines));
+    }
+    public bool AllowReference(Reference r, XYZ p)
+    {	
+        return false;
+    }
+}
